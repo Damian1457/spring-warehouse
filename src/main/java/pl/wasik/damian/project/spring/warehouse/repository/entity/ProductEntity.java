e25ec2122b30000d0acf6e0e8551f4378be6ab76
Damian Wąsik <damianwasik.coach@gmail.com>
2024-08-11 00:16:24 +0200

    Created entity for order, order item, cart, product.

---
+++ src/main/java/pl/wasik/damian/project/spring/warehouse/repository/entity/ProductEntity.java
@@ -0,0 +1,111 @@
+package pl.wasik.damian.project.spring.warehouse.repository.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "PRODUCTS")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int capacity;
+    private String description;
+    private int stock;
+    private double price;
+
+    @Lob
+    private byte[] image;
+
+    @ManyToMany(mappedBy = "products")
+    private Set<CartEntity> carts;
+
+    @OneToMany(mappedBy = "product")
+    private Set<OrderItemEntity> orderItems;
+
+    public ProductEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public Set<CartEntity> getCarts() {
+        return carts;
+    }
+
+    public void setCarts(Set<CartEntity> carts) {
+        this.carts = carts;
+    }
+
+    public Set<OrderItemEntity> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(Set<OrderItemEntity> orderItems) {
+        this.orderItems = orderItems;
+    }
+}
